1e6801929827bad0cf1fc847df4fb6652e3a2e25
Valerio De Filippo <valerio.defilippo@gmail.com>
2022-02-25 15:48:04 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,90 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import usersFile from "./users.json"
+import linkedin from './linkedin.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState(usersFile);
+  const [searchPerson, setSearchPerson] = useState('');
+  const [students, setStudents] = useState('');
+  const [teachers, setTeachers] = useState('');
+  const [campus, setCampus] = useState('');
+
+
+
+  const handleSearchChange = event => {setSearchPerson(event.target.value);
+  const handleSearch = () => {
+    const usersFilter = users.filter(user => {
+    return (
+      user.firstName.toLowerCase().indexOf(searchPerson.toLowerCase()) === 0 ||
+      user.lastName.toLowerCase().indexOf(searchPerson.toLowerCase()) === 0
+      );
+    });
+    setUsers(usersFilter);
+  };
+  }
+
+  const handlestudentsChange = event => {
+    setStudents(event.target.checked);
+    if (!students) setUsers(users.filter(user => user.role === 'student'));
+    if (students) setUsers(usersFile);
+  };
+  const handleteachersChange = event => {
+    setTeachers(event.target.checked);
+    if (!teachers) setUsers(users.filter(user => user.role === 'teacher'));
+    if (teachers) setUsers(usersFile);
+  };
+
+
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div>
+      <input
+        type="text" placeholder="Search" value={searchPerson} onChange={handleSearchChange}
+      />
+      </div>
+      <div> 
+      <input
+        type="checkbox" value={students} onChange={handlestudentsChange}
+      />
+        <label>Student</label>
+      <input
+        type="checkbox" value={teachers} onChange={handleteachersChange}
+      />
+        <label>Teachers</label>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Campus</th>
+            <th>Role</th>
+            <th>Links</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>{user.campus}</td>
+              <td>{user.role}</td>
+              <td>
+                {user.linkedin && (
+                  <a href={user.linkedin}>
+                  <img src={linkedin} alt="LinkedIn icon" height="20" />
+                  </a>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
